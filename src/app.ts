2e7267046bcfdbe1ff16ed2260d75481bbd1303d
butterbeetle <butterbeetle@naver.com>
2023-01-30 23:42:57 +0900

    [4] Class & Interface - public 및 private

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 class Deparment {
-  name: string;
+  public name: string;
+  private employees: string[] = [];
 
   constructor(n: string) {
     this.name = n;
@@ -8,12 +9,28 @@ class Deparment {
   describe(this: Deparment) {
     console.log("Deparment: " + this.name);
   }
+
+  addEmployees(employees: string) {
+    this.employees.push(employees);
+  }
+
+  printEmployeesInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
 const accounting = new Deparment("Accounting");
 
+accounting.addEmployees("Max");
+accounting.addEmployees("Manu");
+
+// accounting.employees[2] = "Anna";
+
 accounting.describe();
+accounting.name = "NEW NAME";
+accounting.printEmployeesInformation();
 
-const accountingCopy = { name: "DUMMY", describe: accounting.describe };
+// const accountingCopy = { name: "DUMMY", describe: accounting.describe };
 
-accountingCopy.describe();
+// accountingCopy.describe();
