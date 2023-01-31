8b5a644b598b3f6f75c7ece31eb5c5bebd982acc
butterbeetle <butterbeetle@naver.com>
2023-01-31 21:25:15 +0900

    [5] 고급 타입 - 인터섹션 타입

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,25 @@
 console.log("Section06");
+interface Admin {
+  name: string;
+  privileges: string[];
+}
+
+interface Employee {
+  name: string;
+  startDate: Date;
+}
+
+interface ElevatedEmployee extends Employee, Admin {}
+
+// type ElevatedEmployee = Admin & Employee;
+
+const e1: ElevatedEmployee = {
+  name: "Max",
+  privileges: ["create-server"],
+  startDate: new Date(),
+};
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric;
