f3c65846ef58e9d99fd23e6245e91228047f7376
butterbeetle <butterbeetle@naver.com>
2023-01-31 22:47:02 +0900

    [6] 제너릭

---
+++ src/section06/app.ts
@@ -0,0 +1,151 @@
+interface Admin {
+  name: string;
+  privileges: string[];
+}
+
+interface Employee {
+  name: string;
+  startDate: Date;
+}
+
+interface ElevatedEmployee extends Employee, Admin {}
+
+// type ElevatedEmployee = Admin & Employee;
+
+const e1: ElevatedEmployee = {
+  name: "Max",
+  privileges: ["create-server"],
+  startDate: new Date(),
+};
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric;
+
+// 함수 오버로드
+function add(a: number, b: number): number;
+function add(a: string, b: string): string;
+function add(a: string, b: number): string;
+function add(a: number, b: string): string;
+function add(a: Combinable, b: Combinable) {
+  // typeof를 사용하는 타입가드
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+const result = add("Max", "Schwarz");
+result.split(" ");
+
+const fetchedUserData = {
+  id: "u1",
+  name: "Max",
+  job: { title: "CEO", description: "My own company" },
+};
+
+console.log(fetchedUserData?.job?.title);
+
+const userInput = undefined;
+
+// userInput = undefined, null 이면 DEFAULT 단순히 "" 처럼 비어있으면 비어있는 값으로 설정해줌
+const storedData = userInput ?? "DEFAULT";
+
+console.log(storedData);
+
+// type UnknownEmployee = Employee | Admin;
+
+// function printEmployeeInformation(emp: UnknownEmployee) {
+//   console.log("Name: " + emp.name);
+//   // 또 다른 타입가드
+//   if ("privileges" in emp) {
+//     console.log("Privileges: " + emp.privileges);
+//   }
+//   if ("startDate" in emp) {
+//     console.log("Start Date: " + emp.startDate);
+//   }
+// }
+
+// printEmployeeInformation({ name: "Manu", startDate: new Date() });
+
+// class Car {
+//   drive() {
+//     console.log("Driving...");
+//   }
+// }
+
+// class Truck {
+//   drive() {
+//     console.log("Driving a Truck...");
+//   }
+//   loadCargo(amount: number) {
+//     console.log("Loading cargo..." + amount);
+//   }
+// }
+
+// type Vehicle = Car | Truck;
+
+// const v1 = new Car();
+// const v2 = new Truck();
+
+// function useVehicle(vehicle: Vehicle) {
+//   vehicle.drive();
+//   //instanceof 타입가드
+//   if (vehicle instanceof Truck) {
+//     vehicle.loadCargo(1000);
+//   }
+// }
+
+// useVehicle(v1);
+// useVehicle(v2);
+
+// interface Bird {
+//   type: "bird";
+//   flyingSpeed: number;
+// }
+// interface Horse {
+//   type: "horse";
+//   runningSpeed: number;
+// }
+
+// type Animal = Bird | Horse;
+
+// function moveAnimal(animal: Animal) {
+//   let speed;
+//   switch (animal.type) {
+//     case "bird":
+//       speed = animal.flyingSpeed;
+//       break;
+//     case "horse":
+//       speed = animal.runningSpeed;
+//       break;
+//   }
+//   console.log("Moving at speed: " + speed);
+// }
+
+// moveAnimal({ type: "bird", flyingSpeed: 10 });
+
+// // 밑 두 줄은 같은 형변환임 맘에드는걸로 일관성을 유지하며 사용할 것
+// // const userInputElemnt = <HTMLInputElement>document.getElementById("user-input")!;
+// // const userInputElemnt = document.getElementById(
+// //   "user-input"
+// // )! as HTMLInputElement;
+// // userInputElemnt.value = "Hi there!"
+
+// // ! 사용 대안
+// const userInputElemnt = document.getElementById("user-input");
+
+// if (userInputElemnt) {
+//   (userInputElemnt as HTMLInputElement).value = "Hi there!";
+// }
+
+// // { email: "Not a valid email", username: "Must start with a charcter!"}
+// interface ErrorContainer {
+//   [prop: string]: string;
+// }
+
+// const errorBag: ErrorContainer = {
+//   email: "Not a valid email",
+//   username: "Must start with a capital charater!",
+// };
