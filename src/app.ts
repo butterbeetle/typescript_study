54c7aa6fb8ff197bbb56841582080f6e34592b01
butterbeetle <butterbeetle@naver.com>
2023-02-02 01:47:06 +0900

    [Section 09] Drag & Drop - Drop할 수 있는 영역 추가

--- src/app.ts
+++ src/app.ts
@@ -192,8 +192,10 @@ class projectItem
 
   @autobind
   dragStartHandler(event: DragEvent): void {
-    console.log(event);
+    event.dataTransfer!.setData("text/plain", this.project.id);
+    event.dataTransfer!.effectAllowed = "move";
   }
+
   dragEndHandler(_: DragEvent): void {
     console.log("DragEnd");
   }
@@ -225,11 +227,16 @@ class ProjectList
   }
 
   @autobind
-  dragOverHandler(_: DragEvent): void {
-    const listEl = this.element.querySelector("ul")!;
-    listEl.classList.add("droppable");
+  dragOverHandler(event: DragEvent): void {
+    if (event.dataTransfer && event.dataTransfer.types[0] === "text/plain") {
+      event.preventDefault();
+      const listEl = this.element.querySelector("ul")!;
+      listEl.classList.add("droppable");
+    }
   }
-  dropHandler(_: DragEvent): void {}
+  dropHandler(event: DragEvent): void {
+    console.log(event.dataTransfer!.getData("text/plain"));
+  }
 
   @autobind
   dragLeaveHandler(_: DragEvent): void {
